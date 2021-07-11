e7ec93c13261367bb961c59a9447ba9ad8cb25a7
fantasworm <tonberry180@gmail.com>
2021-07-11 15:11:52 +0900

    C:/Program Files/Git/edumeet/mp_detection.py add

--- djangoProject/edumeet/consumers.py
+++ djangoProject/edumeet/consumers.py
@@ -1,3 +1,4 @@
+from .mp_detection import fd_image
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 import asyncio
@@ -37,7 +38,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         videodata = json_load['video']
         datalist = list(videodata.values())
         image = np.array(datalist).reshape(480, 640, 4)
+        image, is_absent = fd_image(image)
         print('receive end')
+        print("is_absent : ", is_absent)
         # print(image)
         print("time : ", time.time() - start)
         await self.send(json.dumps({
